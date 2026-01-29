59d66b46fad69d0ba79e861a734845ad247137ff
Daniel Wesloh <22566757+DWesl@users.noreply.github.com>
2026-01-29 17:07:31 +0000

    Load test data as-needed, re-use activation functions, and add types
    Should be closer to the paper's configuration.

--- pytorch_model.py
+++ pytorch_model.py
@@ -5,6 +5,7 @@ import contextlib
 import logging
 import os
 import shutil
+from typing import Callable, Optional
 
 import dask
 import dask.array as da
@@ -30,6 +31,8 @@ dask.config.set(scheduler="synchronous")
 np.set_printoptions(linewidth=shutil.get_terminal_size().columns)
 xr.set_options(display_width=shutil.get_terminal_size().columns)
 
+SLURM_CPUS_PER_TASK = int(os.environ.get("SLURM_CPUS_PER_TASK", 2))
+
 
 def totensor(arr: da.Array) -> torch.Tensor:
     if isinstance(arr, pd.DataFrame):
@@ -44,9 +47,10 @@ class NCFileDataset(Dataset):
 
     def __init__(
         self,
-        data_glob="regression-data/??/all/gdas.f06+inc.202[0-4]??.nc",
-        transform=None,
-        target_transform=None,
+        data_glob: str = "regression-data/??/all/gdas.f06+inc.202[0-4]??.nc",
+        transform: Optional[Callable[[torch.Tensor], torch.Tensor]] = None,
+        target_transform: Optional[Callable[[torch.Tensor], torch.Tensor]] = None,
+        is_train=True,
     ):
         self.ds = xr.open_mfdataset(
             data_glob,
@@ -66,7 +70,12 @@ class NCFileDataset(Dataset):
                 )
                 .load()
             ),
-        ).persist()
+        )
+        if is_train:
+            # Random-access from disk is slow, so load the datasets
+            # that'll be shuffled.  Sequential access should be
+            # decently fast, so can re-read from disk when needed
+            self.ds = self.ds.persist()
         self.ds.coords["isobaricInPa"] = (
             ("isobaricInhPa",),
             self.ds.coords["isobaricInhPa"].values * 100,
@@ -84,7 +93,7 @@ class NCFileDataset(Dataset):
     def __len__(self) -> int:
         return self.ds["o3mr_net_prod"].size
 
-    def __getitem__(self, flat_index: int):
+    def __getitem__(self, flat_index: int) -> tuple:
         ds = self.ds
         dimension_indexes = np.unravel_index(flat_index, ds["t"].shape)
         # _LOGGER.info(
@@ -107,7 +116,7 @@ class NCFileDataset(Dataset):
             target = self.target_transform(target)
         return (regressors, target)
 
-    def __getitems__(self, flat_indexes: list[int]):
+    def __getitems__(self, flat_indexes: list[int]) -> tuple:
         ds = self.ds
         dimension_indexes = np.unravel_index(flat_indexes, ds["t"].shape)
         data = (
@@ -131,7 +140,12 @@ class NCFileDataset(Dataset):
         return (regressors, target)
 
 
-def train_iteration(loader, model, loss_fn, optim):
+def train_iteration(
+    loader: DataLoader,
+    model: nn.Module,
+    loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+    optim,
+):
     """Train the model, running through all the data once."""
     size = len(loader.dataset)
     model.train()
@@ -146,12 +160,16 @@ def train_iteration(loader, model, loss_fn, optim):
         loss.backward()
         optim.step()
         optim.zero_grad()
-        if batch % 100 == 0:
+        if batch % 1000 == 0:
             curr_loss, curr_count = loss.item(), batch * train_batch_size + len(X)
             _LOGGER.info(f"loss: {curr_loss:>7.5g}  [{curr_count:>5d}/{size:>5d}]")
 
 
-def test_iteration(loader, model, loss_fn):
+def test_iteration(
+    loader: DataLoader,
+    model: nn.Module,
+    loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+):
     """Check the model against the test data."""
     # size = len(loader.dataset)
     model.eval()
@@ -193,7 +211,14 @@ def pytorch_distributed():
         torch.distributed.destroy_process_group()
 
 
-def save_checkpoint(model, optimizer, scheduler, epoch, path, mlflow_enabled=False):
+def save_checkpoint(
+    model: nn.Module,
+    optimizer,
+    scheduler,
+    epoch: int,
+    path: str,
+    mlflow_enabled: bool = False,
+):
     if torch.distributed.get_rank() == 0:
         torch.save(
             {
@@ -210,7 +235,7 @@ def save_checkpoint(model, optimizer, scheduler, epoch, path, mlflow_enabled=Fal
         )
 
 
-def load_checkpoint(model, optimizer, scheduler, path):
+def load_checkpoint(model: nn.Module, optimizer, scheduler, path: str) -> int:
     _LOGGER.info(
         "[Rank %d] Loading checkpoint from %s", torch.distributed.get_rank(), path
     )
@@ -225,10 +250,10 @@ def load_checkpoint(model, optimizer, scheduler, path):
 
 
 def create_dataloader(
-    dataset,
-    batch_size=64,
-    num_workers=int(os.environ.get("SLURM_CPUS_PER_TASK", 2)),
-    is_train=True,
+    dataset: Dataset,
+    batch_size: int = 64,
+    num_workers: int = SLURM_CPUS_PER_TASK,
+    is_train: bool = True,
 ):
     sampler = DistributedSampler(dataset, shuffle=is_train)
     # sampler = None
@@ -249,7 +274,7 @@ def create_dataloader(
     )
 
 
-def log_mlflow_artifact(path, artifact_path=None, mlflow_enabled=False):
+def log_mlflow_artifact(path: str, artifact_path=None, mlflow_enabled=False):
     if mlflow_enabled:
         import mlflow
 
@@ -260,22 +285,22 @@ def log_mlflow_artifact(path, artifact_path=None, mlflow_enabled=False):
 
 
 class SirenActivation(nn.Module):
-    def __init__(self, n):
+    def __init__(self, omega):
         super().__init__()
-        self.omega = nn.Parameter(10 * (torch.randn(1) + 1))
+        self.omega = nn.Parameter(torch.Tensor([omega]))
 
     def forward(self, x):
         return torch.sin(self.omega * x)
 
 
 class SirenNN(nn.Module):
-    def __init__(self, hidden_size=512, n_hidden_layers=6):
+    def __init__(self, hidden_size=768, n_hidden_layers=6):
         super().__init__()
-        layers = [nn.Linear(6, hidden_size)]
+        layers = [nn.Linear(6, hidden_size), SirenActivation(6.0)]
+        subsequent_activation = SirenActivation(4.0)
         for _i in range(n_hidden_layers - 1):
-            layers.append(SirenActivation(hidden_size))
             layers.append(nn.Linear(hidden_size, hidden_size))
-        layers.append(SirenActivation(hidden_size))
+            layers.append(subsequent_activation)
         layers.append(nn.Linear(hidden_size, 1))
         self.network = nn.Sequential(*layers)
         self.convert_input = torch.Tensor([1e6, 1e-1, 1e2, 1e-1, 1e-2, 1e-1])
@@ -300,7 +325,7 @@ if __name__ == "__main__":
 
         train_batch_size = 256
         train_dataset = NCFileDataset(
-            "regression-data/??/all/gdas.f06+inc.202[0-4]??.nc"
+            "regression-data/??/all/gdas.f06+inc.202[0-4]??.nc", is_train=True
         )
         # train_dataset = NCFileDataset("regression-data/01/all/gdas.f06+inc.202001.nc")
         train_loader, train_sampler = create_dataloader(
@@ -309,7 +334,7 @@ if __name__ == "__main__":
 
         test_batch_size = 360
         test_dataset = NCFileDataset(
-            "regression-data/??/all/gdas.f06+inc.20[12][95]??.nc"
+            "regression-data/??/all/gdas.f06+inc.20[12][95]??.nc", is_train=False
         )
         # test_dataset = NCFileDataset("regression-data/01/all/gdas.f06+inc.202101.nc")
         test_loader, test_sampler = create_dataloader(
@@ -345,6 +370,9 @@ if __name__ == "__main__":
         optimizer = torch.optim.SGD(siren_model.parameters(), lr=learning_rate)
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.75)
         _LOGGER.info("Learning rate: %.1e\nScheduler: %s", learning_rate, scheduler)
+
+        # Initial metric values, as a baseline
+        test_iteration(test_loader, siren_model, loss_fn, optimizer)
 
         _LOGGER.info("Model defined, starting training\n%s\n", siren_model)
         epochs = 5
